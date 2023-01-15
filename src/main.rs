a00a488bcd9c7735338d217d3f45221ce86f2b09
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2023-01-15 17:51:08 +0100

    feat: add support for single env printing

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ enum ColoringOption {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// List a specific environment variable
+    variable: Option<String>,
+
     /// If the output should be colored
     #[arg(value_enum, long, default_value_t = ColoringOption::AUTO)]
     color: ColoringOption,
@@ -27,9 +30,26 @@ fn main() {
         ColoringOption::AUTO => (),
     }
 
-    let envvars: Vec<(String, String)> = env::vars().collect();
+    match args.variable {
+        Some(_) => print_single_envvar(&args),
+        None => print_all_envvars(&args),
+    }
+}
+
+fn print_all_envvars(_: &Args) {
+    let mut envvars: Vec<(String, String)> = env::vars().collect();
+
+    envvars.sort_by(|a, b| a.0.cmp(&b.0));
 
     for (key, value) in envvars {
         println!("{}={}", key.green(), value.blue());
     }
+}
+
+fn print_single_envvar(args: &Args) {
+    let var = args.variable.as_ref().expect("variable name not specified");
+
+    let value = env::var(var).expect("variable not found");
+
+    println!("{}", value.blue());
 }
