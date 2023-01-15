fc6de6966d181de0f898651a8130de0ef358a5d2
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2023-01-15 18:37:51 +0100

    feat: add table support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use clap::{Parser, ValueEnum};
 use colored::Colorize;
 use std::env;
+use tabled::{
+    format::Format,
+    object::{Columns, Rows},
+    Modify, Style, Table, Tabled, Width,
+};
+
+#[derive(Tabled)]
+#[tabled(rename_all = "PascalCase")]
+struct EnvVar {
+    variable: String,
+    value: String,
+}
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
 enum ColoringOption {
@@ -19,6 +31,10 @@ struct Args {
     /// If the output should be colored
     #[arg(value_enum, long, default_value_t = ColoringOption::AUTO)]
     color: ColoringOption,
+
+    /// Print in a table format
+    #[arg(long)]
+    table: bool,
 }
 
 fn main() {
@@ -36,14 +52,38 @@ fn main() {
     }
 }
 
-fn print_all_envvars(_: &Args) {
-    let mut envvars: Vec<(String, String)> = env::vars().collect();
+fn print_all_envvars(args: &Args) {
+    let mut envvars = env::vars()
+        .map(|v| EnvVar {
+            variable: v.0,
+            value: v.1,
+        })
+        .collect::<Vec<_>>();
 
-    envvars.sort_by(|a, b| a.0.cmp(&b.0));
+    envvars.sort_by(|a, b| a.variable.cmp(&b.variable));
 
-    for (key, value) in envvars {
-        println!("{}={}", key.green(), value.blue());
+    match args.table {
+        true => print_all_table(&envvars),
+        false => print_all_key_value(&envvars),
     }
+}
+
+fn print_all_key_value(envvars: &Vec<EnvVar>) {
+    for var in envvars {
+        println!("{}={}", var.variable.green(), var.value.blue());
+    }
+}
+
+fn print_all_table(envvars: &Vec<EnvVar>) {
+    let mut table = Table::new(envvars);
+    table
+        .with(Style::sharp())
+        .with(Modify::new(Rows::single(0)).with(Format::new(|s| s.bold().to_string())))
+        .with(Modify::new(Rows::new(1..)).with(Width::wrap(80)))
+        .with(Modify::new(Columns::single(0)).with(Format::new(|s| s.green().to_string())))
+        .with(Modify::new(Columns::single(1)).with(Format::new(|s| s.blue().to_string())));
+
+    println!("{}", table);
 }
 
 fn print_single_envvar(args: &Args) {
